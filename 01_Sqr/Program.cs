1eb3dd50e38598fa1c1376382560dad6d66e70bd
Den Beckett <den_local@debian>
2022-11-21 21:02:36 +0300

    fix: add check for null

--- 01_Sqr/Program.cs
+++ 01_Sqr/Program.cs
@@ -1,5 +1,17 @@
-﻿Console.Clear();
-Console.Write("Введите число: ");
-int num = int.Parse(Console.ReadLine());
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+Console.Clear();
+
+Console.WriteLine("Введите число:");
+    
+string input = Console.ReadLine();
+if(input == ""){
+    Console.Write("Ничего не было введено!");
+}
+else{
+int num = Convert.ToInt32(input);
+
 int sqr = num * num;
-Console.WriteLine ("Квадрат числа " + num + " = " + sqr);
+
+Console.Write("Квадрат числа " + num + " = " + sqr);
+}
