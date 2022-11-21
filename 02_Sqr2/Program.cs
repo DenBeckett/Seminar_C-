21d554607a2ad52267bf6ad4718dcd7437a03d06
Den Beckett <den_local@debian>
2022-11-21 21:37:35 +0300

    Sqr2

---
+++ 02_Sqr2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число 1:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 == number1*number1)
+    Console.Write($"Число {number2} - квадрат {number1}!");
+else {
+    Console.WriteLine($"Число {number2} не является квадратом {number1}");
+};
