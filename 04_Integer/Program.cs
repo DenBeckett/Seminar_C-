af1bae8bc634a2413af50c81766c7a01ad790c5a
Den Beckett <den_local@debian>
2022-11-21 22:08:49 +0300

    04 and 05

---
+++ 04_Integer/Program.cs
@@ -0,0 +1,15 @@
+﻿/ Напишите программу, которая на вход принимает одно число(N),а на
+//выходе показывает все целые числа от -N до N
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+
+int num = int.Parse(Console.ReadLine());
+int i=(-1)*num;
+
+while(i <= num)
+{
+    Console.WriteLine(i);
+    i++;
+}
---
+++ 05_LastNumber/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+
+Console.Write("Введите число : ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Последняя цифра числа - ");
+Console.WriteLine(i % 10);
