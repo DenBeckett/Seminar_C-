a876a23cce604ee7b3672f240f10230195f92335
Den Beckett <den_local@debian>
2022-11-22 18:47:10 +0300

    Homework

---
+++ Homework_01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if(a > b) 
+{
+max = a;
+}
+    else 
+    {
+    max = b;
+    }
+Console.WriteLine($"max = {max}");
---
+++ Homework_04/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+while(b <= a)
+{
+    if(b%2 == 0)
+    {
+        Console.Write($"{b} ");
+        b++;
+    }
+    else b++;
+}
+Console.WriteLine();
