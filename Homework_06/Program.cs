2b96c0bc7d3ad246c8033313b1c2f534d0233555
Den Beckett <den_local@debian>
2022-11-26 03:45:29 +0300

    Homework №2

---
+++ Homework_06/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100) Console.WriteLine("Третьей цифры нет");
+else
+{
+    int num1 = num / 100;
+    int num2 = num1 % 10;
+
+    Console.WriteLine($"{num2}");
+}
---
+++ Homework_07/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число");
+int week = Convert.ToInt32(Console.ReadLine());
+
+if (week > 7)
+{
+    Console.WriteLine("Введено неправильное число");
+}
+    else if (week == 6 || week == 7) Console.WriteLine("Выходной");
+    else Console.WriteLine("Рабочий день");
