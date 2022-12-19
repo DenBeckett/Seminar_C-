ea98dfca873a9eeccf88dd95b9bd60d8067cecb5
Den Beckett <den_local@debian>
2022-12-19 22:13:05 +0300

    Seminar 9

---
+++ 37_Interval/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N с помощью рекурсии
+//N = 5 -> "1, 2, 3, 4, 5"
+//N = 6 -> "1, 2, 3, 4, 5, 6"
+//Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N с помощью рекурсии
+//M = 1; N = 5 -> "1, 2, 3, 4, 5"
+//M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+
+Console.Write("Введите M:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+GetSequence(m,n);
+
+void GetSequence(int min, int max)
+{
+    if (min > max) return;
+    Console.Write($"{min} ");
+    GetSequence(min + 1, max);
+}
---
+++ 38_SumNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр с помощью рекурсии.
+//453 -> 12
+//45 -> 9
+
+Console.Write("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+//int sum = 0;
+
+Console.WriteLine(GetSumNumbers(num));
+
+int GetSumNumbers(int num)
+{
+    if (num == 0) return 0;
+    return num%10 + GetSumNumbers(num/10);
+}
